4468a80655c9749aef94e1a7c362606d1a820096
Rakesh Gain <rakeshgain47681@gmail.com>
2022-04-10 16:07:16 +0530

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(BodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 let items = ['Buy food','Cook Food' ,'Eat Food']
-
+let workItems = []
 app.get("/",function(req, res){
     
     // const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
@@ -28,16 +28,27 @@ app.get("/",function(req, res){
     
     let day = today.toLocaleDateString("en-US",options);
 
-    res.render('list', {kindOfDay:day, newItems:items});
+    res.render('list', {listTitle:day, newListitem:items});
 });
 
 
 app.post("/",function(req, res){
+    console.log(req.body)
     let item = req.body.newItem
+    if (req.body.list === "work"){
+        workItems.push(item);
+        res.redirect('/work')
+    }else{
+        items.push(item);
+        res.redirect('/')
+    }
     // console.log(newItem);
     // res.render('list',{items:newItem});
-    items.push(item);
-    res.redirect('/')
+    
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle:"work List", newListitem:workItems})
 });
 
 
